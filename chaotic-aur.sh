83c1f8e0eb041a64ab8e13c5f37f57d3d4876016
Rishabh672003 <jharishabh2k3@gmail.com>
2022-04-20 23:17:09 +0530

    added script to add chaotic aur repo

---
+++ chaotic-aur.sh
@@ -0,0 +1,6 @@
+sudo pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com;
+sudo pacman-key --lsign-key FBA220DFC880C036;
+sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst';
+sudo echo "[chaotic-aur]
+Include = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf
+
