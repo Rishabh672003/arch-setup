51949f2ee5aa4a920e94ae4fffcc2a16ad01f2f9
Rishabh672003 <jharishabh2k3@gmail.com>
2022-04-21 13:04:07 +0530

    final modifications

--- endeavouros-repo.sh
+++ endeavouros-repo.sh
@@ -1,7 +1,7 @@
 sudo echo "[endeavouros]
 SigLevel = PackageRequired
 Include = /etc/pacman.d/endeavouros-mirrorlist" >> /etc/pacman.conf;
-cd /etc/pacman.d/ && wget -c https://gitlab.com/Austcool-Walker/PKGBUILDS/-/raw/master/endeavouros-mirrorlist/endeavouros-mirrorlist;
+cd /etc/pacman.d/ && sudo wget -c https://gitlab.com/Austcool-Walker/PKGBUILDS/-/raw/master/endeavouros-mirrorlist/endeavouros-mirrorlist;
 cd $HOME/ && sudo wget -c https://gitlab.com/Austcool-Walker/PKGBUILDS/-/raw/master/endeavouros-keyring/PKGBUILD && makepkg -si;
 sudo pacman -Syyu
 
