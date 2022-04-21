8957900adc122cde6cbb2837f999443acef89763
Rishabh672003 <jharishabh2k3@gmail.com>
2022-04-21 12:50:50 +0530

    added a script to add endeavour repo to arch

---
+++ endeavouros-repo.sh
@@ -0,0 +1,8 @@
+echo "[endeavouros]
+SigLevel = PackageRequired
+Include = /etc/pacman.d/endeavouros-mirrorlist" >> /etc/pacman.conf;
+cd /etc/pacman.d/ && wget -c https://gitlab.com/Austcool-Walker/PKGBUILDS/-/raw/master/endeavouros-mirrorlist/endeavouros-mirrorlist;
+cd $HOME/ && wget -c https://gitlab.com/Austcool-Walker/PKGBUILDS/-/raw/master/endeavouros-keyring/PKGBUILD && makepkg -si;
+
+
+
