542a2589803020409475877bbe9b4d07089455fb
Rishabh672003 <jharishabh2k3@gmail.com>
2022-04-21 23:16:49 +0530

    added rate-mirrors app to rate mirrors

---
+++ yay/rate-mirrors/rate-mirrors-arch.sh
@@ -0,0 +1,11 @@
+git clone https://aur.archlinux.org/rate-mirrors-bin && cd rate-mirrors-bin && makepkg -si;
+
+export TMPFILE="$(mktemp)"; \
+	 sudo true; \
+	 rate-mirrors --allow-root --save=$TMPFILE arch --max-delay=21600 \
+      && sudo mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist-backup \
+      && sudo mv $TMPFILE /etc/pacman.d/mirrorlist \
+
+
+
+
