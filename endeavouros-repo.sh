a3ee60997cca855d08b93335e3f047133f7d9a35
Rishabh672003 <jharishabh2k3@gmail.com>
2022-04-21 12:52:03 +0530

    modified the script

--- endeavouros-repo.sh
+++ endeavouros-repo.sh
@@ -3,6 +3,6 @@ SigLevel = PackageRequired
 Include = /etc/pacman.d/endeavouros-mirrorlist" >> /etc/pacman.conf;
 cd /etc/pacman.d/ && wget -c https://gitlab.com/Austcool-Walker/PKGBUILDS/-/raw/master/endeavouros-mirrorlist/endeavouros-mirrorlist;
 cd $HOME/ && wget -c https://gitlab.com/Austcool-Walker/PKGBUILDS/-/raw/master/endeavouros-keyring/PKGBUILD && makepkg -si;
-
+sudo pacman -Syyu
 
 
