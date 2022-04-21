b32c22a0b53757d0f18a9ffb4562abf9ef495411
Rishabh672003 <jharishabh2k3@gmail.com>
2022-04-21 13:27:06 +0530

    pipe echo into tee instead of using >>

--- endeavouros-repo.sh
+++ endeavouros-repo.sh
@@ -1,7 +1,6 @@
-sudo true; \
-sudo echo "[endeavouros]
+echo "[endeavouros]
 SigLevel = PackageRequired
-Include = /etc/pacman.d/endeavouros-mirrorlist" >> /etc/pacman.conf;
+Include = /etc/pacman.d/endeavouros-mirrorlist" | sudo tee -a /etc/pacman.conf;
 cd /etc/pacman.d/ && sudo wget -c https://gitlab.com/Austcool-Walker/PKGBUILDS/-/raw/master/endeavouros-mirrorlist/endeavouros-mirrorlist;
 cd $HOME/ && sudo wget -c https://gitlab.com/Austcool-Walker/PKGBUILDS/-/raw/master/endeavouros-keyring/PKGBUILD && makepkg -si;
 sudo pacman -Syyu
