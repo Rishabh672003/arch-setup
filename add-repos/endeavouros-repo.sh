1b43b86d2fc1f7f42be90a27fdbcec191c2b054f
Rishabh672003 <jharishabh2k3@gmail.com>
2022-04-22 23:02:14 +0530

    modified the script and made it proper

--- add-repos/endeavouros-repo.sh
+++ add-repos/endeavouros-repo.sh
@@ -1,8 +1,5 @@
+cd /etc/pacman.d/ && sudo wget -c https://gitlab.com/Austcool-Walker/PKGBUILDS/-/raw/master/endeavouros-mirrorlist/endeavouros-mirrorlist &&
+cd $HOME/ && sudo wget -c https://gitlab.com/Austcool-Walker/PKGBUILDS/-/raw/master/endeavouros-keyring/PKGBUILD && makepkg -si; sudo pacman -Syyu
 echo "[endeavouros]
 SigLevel = PackageRequired
-Include = /etc/pacman.d/endeavouros-mirrorlist" | sudo tee -a /etc/pacman.conf;
-cd /etc/pacman.d/ && sudo wget -c https://gitlab.com/Austcool-Walker/PKGBUILDS/-/raw/master/endeavouros-mirrorlist/endeavouros-mirrorlist;
-cd $HOME/ && sudo wget -c https://gitlab.com/Austcool-Walker/PKGBUILDS/-/raw/master/endeavouros-keyring/PKGBUILD && makepkg -si; sudo pacman -Syyu
-
-
-
+Include = /etc/pacman.d/endeavouros-mirrorlist" | sudo tee -a /etc/pacman.conf
