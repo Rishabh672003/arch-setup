3cd4ca0648dfa677a15dbd20693f97759cbee2a5
Rishabh672003 <jharishabh2k3@gmail.com>
2022-04-22 22:59:38 +0530

    modified the dcript to use && instead of ;

--- add-repos/chaotic-aur.sh
+++ add-repos/chaotic-aur.sh
@@ -1,7 +1,7 @@
-sudo true: \
-sudo pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com;
-sudo pacman-key --lsign-key FBA220DFC880C036;
-sudo pacman --needed --noconfirm -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst';
+sudo true;
+sudo pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com &&
+sudo pacman-key --lsign-key FBA220DFC880C036 &&
+sudo pacman --needed --noconfirm -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'&&
 sudo echo "[chaotic-aur]
 Include = /etc/pacman.d/chaotic-mirrorlist" | sudo tee -a /etc/pacman.conf
 
