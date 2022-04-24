63e46b212669ebc2b9f660f667bd21179a3865c0
Rishabh672003 <jharishabh2k3@gmail.com>
2022-04-24 20:46:37 +0530

    added wget in script

--- add-repos/endeavouros-repo.sh
+++ add-repos/endeavouros-repo.sh
@@ -1,3 +1,4 @@
+yay -S --needed --noconfirm wget &&
 cd /etc/pacman.d/ && sudo wget -c https://gitlab.com/Austcool-Walker/PKGBUILDS/-/raw/master/endeavouros-mirrorlist/endeavouros-mirrorlist &&
 cd $HOME/ && sudo wget -c https://gitlab.com/Austcool-Walker/PKGBUILDS/-/raw/master/endeavouros-keyring/PKGBUILD && makepkg -si &&
 echo "[endeavouros]
