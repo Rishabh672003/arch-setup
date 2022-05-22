ee3330b22508af1d96a6185164d6af1d0e889652
Rishabh <jharishabh2k3@gmail.com>
2022-05-22 21:07:38 +0530

    removed the endeavour script

--- add-repos/endeavouros-repo.sh
+++
@@ -1,7 +0,0 @@
-yay -S --needed --noconfirm wget &&
-cd /etc/pacman.d/ && sudo wget -c https://gitlab.com/Austcool-Walker/PKGBUILDS/-/raw/master/endeavouros-mirrorlist/endeavouros-mirrorlist &&
-cd $HOME/ && sudo wget -c https://gitlab.com/Austcool-Walker/PKGBUILDS/-/raw/master/endeavouros-keyring/PKGBUILD && makepkg -si &&
-echo "[endeavouros]
-SigLevel = PackageRequired
-Include = /etc/pacman.d/endeavouros-mirrorlist" | sudo tee -a /etc/pacman.conf;
-sudo pacman -Syyu
